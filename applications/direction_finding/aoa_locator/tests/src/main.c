64f9d07745f1325ee5a42765178a4a1a59c8f040
Piotr Pryga <piotr.pryga@nordicsemi.no>
2020-09-18 10:33:54 -0700

    applications:direction_finding:aoa_locator: provide unitests subproject in the sample
    Give possiblity to implement and run unit tests for code implemented
    the aoa_locator sample.
    
    Jira: DEB-159
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

---
+++ applications/direction_finding/aoa_locator/tests/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#if !defined(CONFIG_CPU_CORTEX_M)
+  #error project can only run on Cortex-M
+#endif
+
+#include <ztest.h>
+
+
+void test_main(void)
+{
+
+}
